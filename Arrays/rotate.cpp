2be52348d1ce434bf590675dd3c441e1897da740
Subhani-syed <subhani6352@gmail.com>
2022-03-06 14:49:15 +0530

    Easy Approach

--- Arrays/rotate.cpp
+++ Arrays/rotate.cpp
@@ -9,13 +9,23 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-    int temp = arr[n-1];
-    int curr;
-    for(int i=0;i<n;i++){
-        curr = arr[i];
-        arr[i] = temp;
-        temp = curr;
+
+    int temp;
+    temp = arr[n-1];
+    for(int i=n-1;i>=0;i--)
+    {
+        arr[i] = arr[i-1];
     }
+    arr[0] = temp;
+
+    // Another Approach
+    // int temp = arr[n-1];
+    // int curr;
+    // for(int i=0;i<n;i++){
+    //     curr = arr[i];
+    //     arr[i] = temp;
+    //     temp = curr;
+    // }
 
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
